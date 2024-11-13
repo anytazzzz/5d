05cba478d978eea817df6a8432210611f92a8504
anytazzzz <igorcernienko30@gmail.com>
2024-11-13 23:42:44 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var=("чашка",'чай',9)
+print(immutable_var)
+
+#immutable_var[1]='лимон'
+#print(immutable_var)
+
+mutable_list=["чашка",'чай',9]
+print(mutable_list)
+mutable_list[1]='лимон'
+print(mutable_list)
+
